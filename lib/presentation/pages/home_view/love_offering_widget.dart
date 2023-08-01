bcfcf20a6c4865e39c306d4842fb042c73d68ae7
shoh <komiljonovshohjahon@gmail.com>
2023-08-02 04:40:40 +0900

    finish home page

--- lib/presentation/pages/home_view/love_offering_widget.dart
+++ lib/presentation/pages/home_view/love_offering_widget.dart
@@ -39,10 +39,37 @@ class _LoveOfferingContainer extends StatelessWidget {
         height: 200.h,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8.r),
-            color: Colors.white,
+            gradient: LinearGradient(colors: [
+              Color(0xFF8C6924),
+              Color(0xFFf9ad34),
+            ]),
+            // color: const Color(0xFFF9AF34),
             image: const DecorationImage(
+                alignment: Alignment.centerRight,
                 image: AssetImage("assets/images/family_image.png"),
-                fit: BoxFit.cover)),
+                fit: BoxFit.contain)),
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Love Offer Now",
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(color: Colors.white),
+              ),
+              SizedBox(height: 8.h),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Give Now"),
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Color(0xFFf9ad34),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.r)))),
+            ]),
       ),
     );
   }
